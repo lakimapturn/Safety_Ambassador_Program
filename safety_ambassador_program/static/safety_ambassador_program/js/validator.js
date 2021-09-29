e5533e9d5746474cd0fbe342e07bad20a112ea5c
lakimapturn <venomizer11@gmail.com>
2021-09-30 00:43:18 +0400

    added confetti

--- safety_ambassador_program/static/safety_ambassador_program/js/validator.js
+++ safety_ambassador_program/static/safety_ambassador_program/js/validator.js
@@ -10,7 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function verifyAnswer(question) {
-    const val = document.getElementById(question).value;
+    const questionDiv = document.getElementById(question);
+    const val = questionDiv.value;
+    let correctAnswer = "";
     // if (val.length > 3 && val.length < 20) {
     //     document.getElementById("next-button").disabled = false;
     //     document.getElementById("invalid-input-msg").style.opacity = 0;
@@ -19,10 +21,22 @@ function verifyAnswer(question) {
     //     document.getElementById("invalid-input-msg").style.opacity = 1;
     //     document.getElementById("next-button").disabled = true;
     // }
-    console.log(val);
-    if (val == document.getElementById("correct-answer").value) {
-        document.getElementById("invalid-input-msg").style.opacity = 0;
+    for (let i = 0; i < questionDiv.children.length; i++) {
+        if (questionDiv.children[i].id == "correct-answer") {
+            correctAnswer = questionDiv.children[i].innerHTML;
+            break;
+        }
     }
+    if (val == correctAnswer) {
+        document.getElementById("invalid-input-msg").style.opacity = 0;
+        const carouselInner = document.getElementById("quiz-div").getElementsByClassName("carousel-inner")[0];
+        for(let i = 1; i < 20; i++) {
+            let confetti = document.createElement('div');
+            confetti.className += "confetti";
+            questionDiv.parentNode.insertBefore(confetti, questionDiv)
+        }
+    }
+    // fix the wrong answer error and the archives page
     else {
         document.getElementById("invalid-input-msg").style.opacity = 1;
     }
