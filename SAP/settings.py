3417c3b9e16c4d582ed4a4e027ccd4f4219c4929
lakimapturn <venomizer11@gmail.com>
2022-12-13 23:07:16 +0400

    fixed db error

--- SAP/settings.py
+++ SAP/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'SAP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd6t9rqdrg369o',
-        'HOST': 'ec2-34-233-114-40.compute-1.amazonaws.com',
-        'PORT': 5432,
-        'PASSWORD': 'e6916d8905fba8c2bf407b72bed9333aa928623f021425eb306509eda62100b0',
-        'USER': 'pcionpsylzbeti',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'd6t9rqdrg369o',
+        # 'HOST': 'ec2-34-233-114-40.compute-1.amazonaws.com',
+        # 'PORT': 5432,
+        # 'PASSWORD': 'e6916d8905fba8c2bf407b72bed9333aa928623f021425eb306509eda62100b0',
+        # 'USER': 'pcionpsylzbeti',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -138,4 +138,4 @@ AUTH_USER_MODEL = 'safety_ambassador_program.User'
 LOGIN_URL = 'authentication_page'
 
 # Default Auto Field
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
